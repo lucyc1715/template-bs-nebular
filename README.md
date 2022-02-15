b7fac8a017945d1bb6481803f7d128e433e909d0
Lucy <lucychen.biz@gmail.com>
2022-02-15 23:47:13 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TemplateBsNebular
+
+This template is based on Angular 12 and Nebular
+
+## 3-party-Library
+- bootstrap 5: to use RWD
+- moment.js
+- validate.js
+- ngx-lottie
+
+## Author
+Lucy Chen
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    LottieModule.forRoot({ player: playerFactory })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
